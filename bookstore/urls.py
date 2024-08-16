62e2051449b13bf14d85b3e6fd9c1567262e8dc0
Camila Cirne <camila.cirne22@gmail.com>
2024-08-15 23:32:58 -0300

    deploy config

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from bookstore import views
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
@@ -25,4 +26,6 @@ urlpatterns = [
     re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
     re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path("update_server/", views.update, name="update"),
+    path('hello/', views.hello_world, name='hello_world'),
 ]
